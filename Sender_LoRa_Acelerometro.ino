c10b13163102d350ebf8f7eb85508526f3519caf
Manna-Evee <Manna-Evee>
2018-06-23 17:47:00 -0300

    commit inicial

---
+++ Sender_LoRa_Acelerometro.ino
@@ -0,0 +1,85 @@
+/* 
+  Basic test program, send date at the BAND you seted.
+  
+  by Aaron.Lee from HelTec AutoMation, ChengDu, China
+  成都惠利特自动化科技有限公司
+  www.heltec.cn
+  
+  this project also realess in GitHub:
+  https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+*/
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>
+
+// Pin definetion of WIFI LoRa 32
+// HelTec AutoMation 2017 support@heltec.cn 
+
+#define SCK     5    // GPIO5  -- SX127x's SCK
+#define MISO    19   // GPIO19 -- SX127x's MISO
+#define MOSI    27   // GPIO27 -- SX127x's MOSI
+#define SS      18   // GPIO18 -- SX127x's CS
+#define RST     14   // GPIO14 -- SX127x's RESET
+#define DI0     26   // GPIO26 -- SX127x's IRQ(Interrupt Request)
+
+#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+#define PABOOST true
+
+//MPU6050 mpu;
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(250000);
+  pinMode(25,OUTPUT);
+  
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Tmp=Tmp/340.00+36.53;
+  String packetSend = String(AcX)+" "+ String(AcY)+" "+ String(AcZ)+" "+ String(GyX)+" "+ String(GyY)+" "+ String(GyZ)+" "+String(Tmp);
+  Serial.println(packetSend);
+  // send packet
+  LoRa.beginPacket();
+//  LoRa.print(String(AcX));
+//  LoRa.print(String(AcY));
+//  LoRa.print(String(AcZ));
+//  LoRa.print(String(GyX));
+//  LoRa.print(String(GyY));
+//  LoRa.print(String(GyZ));
+  LoRa.print(packetSend);
+  LoRa.endPacket();
+
+  
+  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
+
---
+++ rtplot.py
@@ -0,0 +1,93 @@
+import serial
+import numpy as np
+from matplotlib import pyplot as plt
+from time import time
+
+# If you're not using Linux, you'll need to change this
+# check the Arduino IDE to see what serial port it's attached to
+ser = serial.Serial('/dev/ttyUSB1', 115200)
+
+# set plot to animated
+plt.ion() 
+
+start_time = time()
+timepoints = []
+ydataX = []
+ydataY = []
+ydataZ = []
+yrange = [-20000,20000]
+view_time = 60 # seconds of data to view at once
+duration = 24 # total seconds to collect data
+
+fig1 = plt.figure()
+# http://matplotlib.org/users/text_props.html
+fig1.suptitle('Received Data', fontsize='18', fontweight='bold')
+plt.xlabel('time, seconds', fontsize='14', fontstyle='italic')
+plt.ylabel('acceleration', fontsize='14', fontstyle='italic')
+plt.axes().grid(True)
+lineX, = plt.plot(ydataX,marker='.',markersize=4,linestyle='--',markerfacecolor='red')
+lineY, = plt.plot(ydataY,marker='.',markersize=4,linestyle='--',markerfacecolor='blue')
+lineZ, = plt.plot(ydataZ,marker='.',markersize=4,linestyle='--',markerfacecolor='green')
+plt.ylim(yrange)
+plt.xlim([0,view_time])
+
+# flush any junk left in the serial buffer
+ser.flushInput()
+# ser.reset_input_buffer() # for pyserial 3.0+
+run = True
+
+# collect the data and plot a moving frame
+while run:
+    ser.reset_input_buffer()
+    data = ser.readline().split(' ')
+    print data
+    
+    # sometimes the incoming data is garbage, so just 'try' to do this
+    try:
+        # store the entire dataset for later
+        ydataX.append(float(data[0]))
+        ydataY.append(float(data[1]))
+        ydataZ.append(float(data[2]))
+
+        timepoints.append(time()-start_time)
+        current_time = timepoints[-1]
+        
+        # update the plotted data
+        lineX.set_xdata(timepoints)
+        lineX.set_ydata(ydataX)
+
+        lineY.set_xdata(timepoints)
+        lineY.set_ydata(ydataY)
+
+        lineZ.set_xdata(timepoints)
+        lineZ.set_ydata(ydataZ)
+        
+        # slide the viewing frame along
+        if current_time > view_time:
+            plt.xlim([current_time-view_time,current_time])
+            
+        # when time's up, kill the collect+plot loop
+        # if timepoints[-1] > duration: run=False
+    
+    # if the try statement throws an error, just do nothing
+    except: pass
+    
+    # update the plot
+    fig1.canvas.draw()
+
+# plot all of the data you collected
+fig2 = plt.figure()
+# http://matplotlib.org/users/text_props.html
+fig2.suptitle('complete data trace', fontsize='18', fontweight='bold')
+plt.xlabel('time, seconds', fontsize='14', fontstyle='italic')
+plt.ylabel('potential, volts', fontsize='14', fontstyle='italic')
+plt.axes().grid(True)
+
+plt.plot(timepoints, ydataX,marker='o',markersize=4,linestyle='--',markerfacecolor='red')
+plt.plot(timepoints, ydataY,marker='o',markersize=4,linestyle='--',markerfacecolor='red')
+plt.plot(timepoints, ydataZ,marker='o',markersize=4,linestyle='--',markerfacecolor='red')
+
+plt.ylim(yrange)
+fig2.show()
+
+ser.close()
